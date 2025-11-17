b6dc88b5d687bcfe37297bdcfe4136ad6a9eccb7
Ratul0012 <fahimchowdhury0170@gmail.com>
2025-11-17 11:41:29 +0600

    Update Even.php

--- MID/LabTask2/Even.php
+++ MID/LabTask2/Even.php
@@ -3,11 +3,40 @@
 <head>
     <title>Participant Registration</title>
 </head>
+<style>
+    
+    input[type=text], input[type=number], input[type=password] {
+        width: 300px;
+        padding: 10px;
+        margin: 5px 0 15px 0;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+    }
+    button {
+        background-color: #352d90ff;
+        color: white;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }
 
+    fieldset {
+        margin: auto;
+        width: 50%;
+        border: 2px solid #352d90ff;
+        border-radius: 5px;
+        padding: 20px;
+        
+    }
+    
+</style>
+<fieldset>
 <body>
     <center>
     <h1>Participant Registration</h1>
     </center>
+    
     <form>
 
    Full Name :<br>
@@ -27,8 +56,10 @@
     <br><br>
     <button type="submit">Register</button>
     </form>
+    
 </body>
-
+</fieldset>
+</html>
 
 
 
