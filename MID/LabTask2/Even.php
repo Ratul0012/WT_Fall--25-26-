2ec19ccefc6d979e6abc1faaf87d3a8c3c5ec857
Ratul0012 <fahimchowdhury0170@gmail.com>
2025-11-17 11:30:42 +0600

    Update Even.php

--- MID/LabTask2/Even.php
+++ MID/LabTask2/Even.php
@@ -2,5 +2,34 @@
 <html>
 <head>
     <title>Participant Registration</title>
-    
+</head>
+
+<body>
+    <center>
+    <h1>Participant Registration</h1>
+    </center>
+    <form>
+
+   Full Name :<br>
+   <input type="text" >
+    <br>
+    Email :<br>
+    <input type="text" >
+    <br>
+    Phone Number :<br>
+    <input type="number" > 
+    <br>
+    Password:<br>
+    <input type="password" >
+    <br>
+    Confirm Password:<br>
+    <input type="password" >
+    <br><br>
+    <button type="submit">Register</button>
+    </form>
+</body>
+
+
+
+
     
